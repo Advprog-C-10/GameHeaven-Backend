27540daa06573291dd7e288d5463a26a9785fb16
MahogaHeryasa <hery.heryasa@gmail.com>
2024-04-18 23:50:08 +0700

    initialize

---
+++ src/main/java/id/ac/ui/cs/advprog/gameheavenbackend/GameHeavenBackendApplication.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.gameheavenbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameHeavenBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameHeavenBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/id/ac/ui/cs/advprog/gameheavenbackend/GameHeavenBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.gameheavenbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameHeavenBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
